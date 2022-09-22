18bbe046a4eeaef043e14c415f803b5cd55eb2dc
Tamme Dittrich <td-github@orangecar.org>
2022-09-22 12:52:20 +0200

    Update benchmark to run mutliple sizes

--- benches/histogram.rs
+++ benches/histogram.rs
@@ -1,22 +1,42 @@
 use std::collections::hash_map::RandomState;
 
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 
 use histongram::Histogram;
 
 const APACHE: &str = include_str!("../LICENSE-APACHE");
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    #[cfg(feature = "fxhash")]
-    c.bench_function("fill_fxhash", |b| {
-        b.iter(|| Histogram::<_, fxhash::FxBuildHasher>::from_owned_iter(black_box(APACHE).chars()))
-    });
-    c.bench_function("fill_ahash", |b| {
-        b.iter(|| Histogram::<_, ahash::RandomState>::from_owned_iter(black_box(APACHE).chars()))
-    });
-    c.bench_function("fill_std", |b| {
-        b.iter(|| Histogram::<_, RandomState>::from_owned_iter(black_box(APACHE).chars()))
-    });
+    let mut group = c.benchmark_group("chars");
+
+    for len in [10, 100, APACHE.chars().count()] {
+        group.throughput(Throughput::Elements(len as u64));
+
+        #[cfg(feature = "fxhash")]
+        {
+            group.bench_with_input(
+                BenchmarkId::new("fill_fxhash", len),
+                &APACHE[..len],
+                |b, input| {
+                    b.iter(|| Histogram::<_, fxhash::FxBuildHasher>::from_owned_iter(input.chars()))
+                },
+            );
+        }
+        group.bench_with_input(
+            BenchmarkId::new("fill_ahash", len),
+            &APACHE[..len],
+            |b, input| {
+                b.iter(|| Histogram::<_, ahash::RandomState>::from_owned_iter(input.chars()))
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("fill_std", len),
+            &APACHE[..len],
+            |b, input| b.iter(|| Histogram::<_, RandomState>::from_owned_iter(input.chars())),
+        );
+    }
+
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
